0fa345e91ca5c0f314065bb8b21c65cf74ef9ffb
mustafa siam <mustafasiam357@gmail.com>
2025-07-23 11:29:40 +0600

    client site initialize

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1 className='text-4xl text-amber-700'>Vite + React</h1>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,12 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from "../App";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+]);
+export default router
