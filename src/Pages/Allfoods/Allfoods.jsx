175ceb06933784bd746f66056e35163ef61e1c82
mustafa siam <mustafasiam357@gmail.com>
2025-08-06 01:13:09 +0600

    added pagination

--- src/Pages/Allfoods/Allfoods.jsx
+++ src/Pages/Allfoods/Allfoods.jsx
@@ -1,23 +1,70 @@
 import React, { useEffect, useState } from 'react';
 import useAxiosSecure from '../../Hooks/UseAxiosSecure';
 import Topfood from '../Home/Topfood';
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
 
 const Allfoods = () => {
-    const [allfoods,setallfoods]=useState([])
-    const axiosinstance=useAxiosSecure()
-    useEffect(()=>{
-        axiosinstance.get('/allcuisin')
-        .then(res=>{
-            setallfoods(res.data)
-        })
-    },[axiosinstance])
-    return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-2'>
-            {
-                 allfoods.map(topfood=><Topfood key={topfood._id} topfood={topfood}></Topfood>)
-            }
+  const [allfoods, setallfoods] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [page,setpage]=useState(1)
+  const [totalpage,settotalpage]=useState(1)
+  const limit=9;
+  const axiosinstance = useAxiosSecure();
+
+  useEffect(() => {
+    axiosinstance.get(`/allcuisin?search=${searchTerm}&page=${page}&limit${limit}`)
+      .then(res => {
+        setallfoods(res.data.foods);
+        settotalpage(res.data.totalpage)
+        console.log(res.data.totalpage)
+      });
+  }, [axiosinstance,searchTerm,page]);
+
+
+  return (
+    <>
+      <div className='flex justify-end'>
+        <label className="input flex items-center gap-2">
+          <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor">
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input
+            type="search"
+            required
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) =>{
+                 setSearchTerm(e.target.value);
+                 setpage(1);
+            }}     
+          />
+        </label>
+      </div>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-2'>
+       {allfoods.length > 0 ? (
+          allfoods.map(item => <Topfood key={item._id} topfood={item} />)
+        ) : (
+          <p className="col-span-full text-center text-gray-500">No foods found</p>
+        )}
+      </div>
+       {totalpage > 1 && (
+        <div className='flex justify-center mt-6'>
+          <Pagination
+            count={totalpage}
+            page={page}
+            onChange={(e, value) => setpage(value)}
+            color="secondary"
+            size="large"
+          />
         </div>
-    );
+      )}
+    </>
+  );
 };
 
 export default Allfoods;
--- src/Pages/Foodgallery/Foodgallery.jsx
+++ src/Pages/Foodgallery/Foodgallery.jsx
@@ -4,9 +4,15 @@ import LightGallery from 'lightgallery/react';
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lg-autoplay.css'
+import 'lightgallery/css/lg-fullscreen.css'
+import 'lightgallery/css/lg-rotate.css'
+//plugins
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import lgZoom from 'lightgallery/plugins/zoom';
-
+import lgAutoplay from 'lightgallery/plugins/autoplay'
+import lgFullscreen from 'lightgallery/plugins/fullscreen'
+import lgRotate from 'lightgallery/plugins/rotate'
 const Foodgallery = () => {
    const onInit = () => {
         console.log('lightGallery has been initialized');
@@ -16,25 +22,26 @@ const Foodgallery = () => {
 
   useEffect(() => {
     axiosinstance.get("/allcuisin").then((res) => {
-      setAllFoods(res.data);
+      setAllFoods(res.data.foods);
     });
   }, [axiosinstance]);
 
 
   return (
     <>
- <div className="columns-1 gap-2 md:columns-3  p-4">
+ <div className="columns-1 gap-2 md:columns-2 lg:columns-3  p-4">
             <LightGallery
                 onInit={onInit}
                 speed={500}
-                plugins={[lgThumbnail, lgZoom]}
+                plugins={[lgThumbnail, lgZoom,lgAutoplay,lgFullscreen,lgRotate]}
+                 mode="lg-fade"
             >
             {
               allfoods.map((food,idx)=>{
                 return(
                     <a href={food.image} key={idx}>
                     <img alt={food.name} src={food.image} 
-                     className="w-full rounded-xl mb-2"
+                     className="w-full rounded-xl mb-2 transition duration-300 hover:opacity-90 hover:scale-[1.01]"
                     />
                 </a>
                 )
